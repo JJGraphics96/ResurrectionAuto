6d37c26a6aa312b374d7b226f1b2cb59981cc8f1
JJGraphics96 <jesse96jimenez@gmail.com>
2025-07-08 15:10:02 -0700

    Delete projectscripts directory

--- projectscripts/nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggle = document.getElementById('nav-toggle');
-    const links = document.getElementById('nav-links');
-
-    toggle.addEventListener('click', () => {
-        links.classList.toggle('show');
-    });
-});
